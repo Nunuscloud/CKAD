22b4b15a0db802d9c7bbfb8dd294ae55bd44b823
Nunu <109891153+Nunuscloud@users.noreply.github.com>
2023-03-13 19:38:02 +0900

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "main_vol_size" {
+  type = number
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
